b109364e4b90e17b21a9118601161ac2827833ea
Gabriel Da Silva Freitas <72677685+Freitasbtw@users.noreply.github.com>
2023-05-23 10:26:09 -0300

    Update Sprint_Lab.java
    23/05 - Adicionado verificador de potência a cada passo.

--- Sprint_Lab.java
+++ Sprint_Lab.java
@@ -13,10 +13,6 @@ public class Sprint_Lab {
 		System.out.println("Seja muito bem vindo ao labirinto do robô!!");
 		System.out.println("Digite a potência: ");
 		potencia = e.nextInt();
-		while (potencia < 98) {
-			System.out.println("Potência insuficiente, por favor digite um número maior.");
-			potencia = e.nextInt();
-		}
 		System.out.println("Robô carregado com: " + potencia);
 		System.out.println("Vamos lá!!");
 		System.out.println("Digite a direção que o robô vai andar: ");
@@ -32,6 +28,13 @@ public class Sprint_Lab {
 			passos = e.nextInt();
 			distancia = distancia+passos;
 			if (passos == 5) {
+				while (potencia < 10) {
+					System.out.println("Sua bateria está muito baixa.");
+					System.out.println("Você precisa de no mínimo 10 de potência para esse movimento.");
+					System.out.println("Sua potencia atual é de: " + potencia + " Por favor carregue seu robô: ");
+					potencia = potencia + e.nextInt();
+				}
+				potencia = potencia - 10;
 				System.out.println("Você completou a 1º Etapa");
 				System.out.println("Digite a direção que o robô vai andar: ");
 				direcao = e.next();
@@ -44,6 +47,13 @@ public class Sprint_Lab {
 					passos = e.nextInt();
 					distancia = distancia+passos;
 					if (passos == 7) {
+						while (potencia < 14) {
+							System.out.println("Sua bateria está muito baixa.");
+							System.out.println("Você precisa de no mínimo 14 de potência para esse movimento.");
+							System.out.println("Sua potencia atual é de: " + potencia + " Por favor carregue seu robô: ");
+							potencia = potencia + e.nextInt();
+						}
+						potencia = potencia - 14;
 						System.out.println("Você completou a 2º Etapa");
 						System.out.println("Digite a direção que o robô vai andar: ");
 						direcao = e.next();
@@ -56,6 +66,13 @@ public class Sprint_Lab {
 							passos = e.nextInt();
 							distancia = distancia+passos;
 							if (passos == 7) {
+								while (potencia < 14) {
+									System.out.println("Sua bateria está muito baixa.");
+									System.out.println("Você precisa de no mínimo 14 de potência para esse movimento.");
+									System.out.println("Sua potencia atual é de: " + potencia + " Por favor carregue seu robô: ");
+									potencia = potencia + e.nextInt();
+								}
+								potencia = potencia - 14;
 								System.out.println("Você completou a 3º Etapa");
 								System.out.println("Digite a direção que o robô vai andar: ");
 								direcao = e.next();
@@ -68,6 +85,13 @@ public class Sprint_Lab {
 									passos = e.nextInt();
 									distancia = distancia+passos;
 									if (passos == 11) {
+										while (potencia < 22) {
+											System.out.println("Sua bateria está muito baixa.");
+											System.out.println("Você precisa de no mínimo 22 de potência para esse movimento.");
+											System.out.println("Sua potencia atual é de: " + potencia + " Por favor carregue seu robô: ");
+											potencia = potencia + e.nextInt();	
+										}
+										potencia = potencia - 22;
 										System.out.println("Você completou a 4º Etapa");
 										System.out.println("Digite a direção que o robô vai andar: ");
 										direcao = e.next();
@@ -80,6 +104,13 @@ public class Sprint_Lab {
 											System.out.println("Quantos passos o robô vai dar?");
 											passos = e.nextInt();
 											if (passos == 8) {
+												while (potencia < 16) {
+													System.out.println("Sua bateria está muito baixa.");
+													System.out.println("Você precisa de no mínimo 16 de potência para esse movimento.");
+													System.out.println("Sua potencia atual é de: " + potencia + " Por favor carregue seu robô: ");
+													potencia = potencia + e.nextInt();
+												}
+												potencia = potencia - 16;
 												System.out.println("Você completou a 5º Etapa");
 												System.out.println("Digite a direção que o robô vai andar: ");
 												direcao = e.next();
@@ -92,6 +123,13 @@ public class Sprint_Lab {
 													passos = e.nextInt();
 													distancia = distancia+passos;
 													if (passos == 8) {
+														while (potencia < 16) {
+															System.out.println("Sua bateria está muito baixa.");
+															System.out.println("Você precisa de no mínimo 16 de potência para esse movimento.");
+															System.out.println("Sua potencia atual é de: " + potencia + " Por favor carregue seu robô: ");
+															potencia = potencia + e.nextInt();
+														}
+														potencia = potencia - 16;
 														System.out.println("Você completou a 6º Etapa");
 														System.out.println("Digite a direção que o robô vai andar: ");
 														direcao = e.next();
@@ -104,6 +142,13 @@ public class Sprint_Lab {
 															passos = e.nextInt();
 															distancia = distancia+passos;
 															if (passos == 3) {
+																while (potencia < 6) {
+																	System.out.println("Sua bateria está muito baixa.");
+																	System.out.println("Você precisa de no mínimo 6 de potência para esse movimento.");
+																	System.out.println("Sua potencia atual é de: " + potencia + " Por favor carregue seu robô: ");
+																	potencia = potencia + e.nextInt();
+																}
+																potencia = potencia - 6;
 																System.out.println("Parabéns você chegou!!");
 															} else {
 																System.out.println("O robô bateu!");
